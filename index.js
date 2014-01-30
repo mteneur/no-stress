29184915dd2097c7abc9ca4071b95c24fcb4c2ef
mteneur <maxime.teneur@lanetscouade.com>
2014-01-30 15:12:55 +0100

    Add KeepAlive option

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
 	  .usage('[options] <url>')
 	  .option('-a, --amount <n>', 'Total number of persistent connection, Default to 100', parseInt)
 	  .option('-w, --worker <n>', 'Total number of worker (thread), Default to 4', parseInt)
-	  .option('-k, --keep-alive', 'TO DO -- Keep alive connection')
+	  .option('-k, --keep-alive <n>', 'Time to keep alive connection, default is "infinity"', parseInt)
 	  .parse(process.argv);
 
 	if (program.args.length < 1) {
@@ -21,10 +21,12 @@
 	if (!program.amount) {
 		program.amount = 100;
 	}
-
+	if (!program.keepAlive) {
+		program.keepAlive = false;
+	}
 	if (!program.worker) {
 		program.worker = 2;
 	}
-	new workbench(program.amount, program.worker, url);
+	new workbench(program.amount, program.worker, program.keepAlive, url);
 
 })();
